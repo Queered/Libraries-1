888589491b965a4e4b8f58516446e48ffdfbdc74
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2022-06-21 18:25:56 +0200

    Update vozoid-ui.lua

--- vozoid-ui.lua
+++ vozoid-ui.lua
@@ -1111,7 +1111,7 @@ local themes = {
 local themeobjects = {}
 
 local library = utility.table({folder = "vozoiduilib", extension = "vozoid", flags = {}, open = true, keybind = Enum.KeyCode.RightShift, cursor = nil, holder = nil, connections = {}}, true)
-local decode = base64_decode or base64decode or (syn and syn.crypt.base64.decode) or (crypt and crypt.base64decode or (crypt.base64 and crypt.base64.decode))
+local decode = base64_decode or base64decode or (syn and syn.crypt.base64.decode) or (crypt and crypt.base64decode or (crypt and crypt.base64 and crypt.base64.decode))
 library.gradient = decode("iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABuSURBVChTxY9BDoAgDASLGD2ReOYNPsR/+BAfroI7hibe9OYmky2wbUPIOdsXdc1f9WMwppQm+SDGBnUvomAQBH49qzhFEag25869ElzaIXDhD4JGbyoEVxUedN8FKwnfmwhucgKICc+pNB1mZhdCdhsa2ky0FAAAAABJRU5ErkJggg==")
 library.utility = utility
 
