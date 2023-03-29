a58e2dd639fbc6f1c39f08ccafa4089eb0facd57
mickey <59575072+Spoorloos@users.noreply.github.com>
2023-03-29 20:39:32 +0200

    Update sorted_array.lua

--- sorted_array.lua
+++ sorted_array.lua
@@ -18,11 +18,11 @@ function SortedArray:Push(value: any)
     table.insert(self._array, value);
 end
 
-function SortedArray:Pop(idx: number?): any
+function SortedArray:Pop(idx: number?): any | nil
     return table.remove(self._array, idx or 1);
 end
 
-function SortedArray:Peek(idx: number?): any
+function SortedArray:Peek(idx: number?): any | nil
     return self._array[idx or 1];
 end
 
